977fbfea49429c91e9d6c2a7407e76083826ae68
vikrant <vikrant460@gmail.com>
2025-08-25 21:29:12 +0530

    add tiptap editor with basic support for bold, italic, list

--- app/angular/demo/src/app/app.ts
+++ app/angular/demo/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TiptapEditor } from './tiptap-editor/tiptap-editor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TiptapEditor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- app/angular/demo/src/app/tiptap-editor/tiptap-editor.ts
+++ app/angular/demo/src/app/tiptap-editor/tiptap-editor.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Editor } from '@tiptap/core';
+import { FloatingMenu } from '@tiptap/extension-floating-menu';
+import { ListKit } from '@tiptap/extension-list';
+import StarterKit from '@tiptap/starter-kit';
+import { TiptapEditorDirective, TiptapFloatingMenuDirective } from 'ngx-tiptap';
 
 @Component({
   selector: 'app-tiptap-editor',
-  imports: [],
+  imports: [CommonModule, FormsModule, TiptapEditorDirective, TiptapFloatingMenuDirective],
   templateUrl: './tiptap-editor.html',
-  styleUrl: './tiptap-editor.css'
+  styleUrl: './tiptap-editor.css',
+  encapsulation: ViewEncapsulation.None
 })
-export class TiptapEditor {
+export class TiptapEditor implements OnDestroy {
+  private value = '<p>Hello, Tiptap!</p>'; // can be HTML or JSON, see https://www.tiptap.dev/api/editor#content
+  editor = new Editor({
+    content: this.value,
+    extensions: [StarterKit, FloatingMenu,ListKit]
+  });
 
+
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 }
