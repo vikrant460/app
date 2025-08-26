ab7e1067a9faea3ae2b3eb858fc5c76fd54c6a60
vikrant <vikrant460@gmail.com>
2025-08-26 17:00:35 +0530

    add style

--- app/angular/demo/src/app/tiptap-editor/tiptap-editor.ts
+++ app/angular/demo/src/app/tiptap-editor/tiptap-editor.ts
@@ -3,6 +3,7 @@ import { Component, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Editor } from '@tiptap/core';
 import { FloatingMenu } from '@tiptap/extension-floating-menu';
+import Link from '@tiptap/extension-link';
 import { ListKit } from '@tiptap/extension-list';
 import StarterKit from '@tiptap/starter-kit';
 import { TiptapEditorDirective, TiptapFloatingMenuDirective } from 'ngx-tiptap';
@@ -16,7 +17,8 @@ import { TiptapEditorDirective, TiptapFloatingMenuDirective } from 'ngx-tiptap';
 })
 export class TiptapEditor implements OnDestroy {
   private value = '<p>Hello, Tiptap!</p>'; // can be HTML or JSON, see https://www.tiptap.dev/api/editor#content
-  private jsonDoc = {
+  output = '';
+  private paragraphDoc = {
     "type": "doc",
     "content": [
       {
@@ -30,13 +32,64 @@ export class TiptapEditor implements OnDestroy {
       }
     ]
   }
+  private linkDoc = {
+  type: 'doc',
+  content: [
+    {
+      type: 'paragraph',
+      content: [
+        {
+          type: 'text',
+          text: 'Example',
+          marks: [
+            {
+              type: 'link',
+              attrs: {
+                href: 'https://example.com',
+              },
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
   showFloatingMenu = false;
   editor = new Editor({
-    content: this.jsonDoc,
-    extensions: [StarterKit, FloatingMenu, ListKit]
+    content: this.paragraphDoc,
+    extensions: [StarterKit, FloatingMenu],
+    onUpdate: ({ editor }) => {
+    const json = editor.getJSON();
+ 
+    this.output = JSON.stringify(JSON);
+       console.log(this.output)
+  },
   });
+  addLink() {
+    const url = prompt('Enter the URL');
 
+    if (url && this.isValidUrl(url)) {
+      // this.editor
+      //   .chain()
+      //   .focus()
+      //   .extendMarkRange('link')
+      //   .setLink({ href: url })
+      //   .run();
+      this.editor.chain().focus().setLink({ href: url }).run();
+    }
+  }
+  isActive(format: string) {
+    return this.editor.isActive(format);
+  }
 
+  private isValidUrl(str: string) {
+    try {
+      new URL(str);
+      return true;
+    } catch {
+      return false;
+    }
+  }
   insertText(event: Event) {
     const text = (event.target as HTMLTextAreaElement).value;
     if (!text || !this.editor?.isEditable) return
