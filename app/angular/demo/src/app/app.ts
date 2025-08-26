74e3745fa0f403136f66a0229cd2250c239795e7
vikrant <vikrant460@gmail.com>
2025-08-26 17:50:03 +0530

    add ngx editor

--- app/angular/demo/src/app/app.ts
+++ app/angular/demo/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TiptapEditor } from './tiptap-editor/tiptap-editor';
+import { NgxTexteditor } from "./ngx-texteditor/ngx-texteditor";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TiptapEditor],
+  imports: [RouterOutlet, TiptapEditor, NgxTexteditor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
