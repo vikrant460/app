a0f80f9d2871f0c12fe7eccc7ee09c722a7d43be
vikrant <vikrant460@gmail.com>
2025-09-04 21:48:10 +0530

    remove attr

--- app/angular/demo/src/app/boomark-selector/bookmark-schema..ts
+++ app/angular/demo/src/app/boomark-selector/bookmark-schema..ts
@@ -1,34 +1,31 @@
 import { MarkSpec, Schema } from 'prosemirror-model';
-
-import { schema as defaultSchema } from 'ngx-editor/schema'; // import the full default schema
-
+import { schema } from 'ngx-editor/schema';
 const bookmarkMark: MarkSpec = {
-  attrs: { type: {} },
-  inclusive: false,
-  toDOM(mark) {
-    const value = mark.attrs['type'];
-    return [
-      'span',
-      { 'data-bookmark': value, class: 'bookmark' },
-      0 // IMPORTANT: this tells ProseMirror to render the text content here
-    ];
-  },
-  parseDOM: [
-    {
-      tag: 'span[data-bookmark]',
-      getAttrs(dom) {
-        return { type: (dom as HTMLElement).getAttribute('data-bookmark') };
-      },
+    inclusive: false,
+    toDOM(mark) {
+        const value = mark.attrs['type'];
+        return [
+            'span',
+            { 'data-bookmark': value, class: 'bookmark' },
+            0 // IMPORTANT: this tells ProseMirror to render the text content here
+        ];
     },
-  ],
+    parseDOM: [
+        {
+            tag: 'span[data-bookmark]',
+            getAttrs(dom) {
+                return { type: (dom as HTMLElement).getAttribute('data-bookmark') };
+            },
+        },
+    ],
 };
 
 
 // take existing marks + add bookmark
-const marks = defaultSchema.spec.marks.addToEnd('bookmark', bookmarkMark);
+const marks = schema.spec.marks.addToEnd('bookmark', bookmarkMark);
 
 // reuse all default nodes
-const nodes = defaultSchema.spec.nodes;
+const nodes = schema.spec.nodes;
 
 export const bookmarkSchema = new Schema({ nodes, marks });
 
