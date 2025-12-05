be00e27aebcf4ea2a1e7c188043b035d6b02c1fb
vikrant <vikrant460@gmail.com>
2025-12-05 11:30:10 +0530

    use collection expression in test data

--- app/dotnet/sample1.test/CrackPassword_Day1_Test.cs
+++ app/dotnet/sample1.test/CrackPassword_Day1_Test.cs
@@ -17,22 +17,22 @@ namespace sample1.test
             var result = await cracker.CrackPasswordV1(moves);
             Assert.Equal(expectedPassword, result);
         }
-        public static readonly IEnumerable<object[]> _testLeftMove = new List<object[]>
-        {
-            new object[] { new List<Move> { new Move(Direction.Left, 1) }, 2, 0, 0 },
-            new object[] { new List<Move> { new Move(Direction.Left, 2) }, 2, 0, 0 },
-            new object[] { new List<Move> { new Move(Direction.Left, 3) }, 2, 0, 1 },
-            new object[] { new List<Move> { new Move(Direction.Left, 7) }, 2, 1, 3 },
-            new object[] { new List<Move> { new Move(Direction.Left, 8) }, 2, 1, 2 },
-        };
-        public static readonly IEnumerable<object[]> _testRighttMove = new List<object[]>
-        {
-            new object[] { new List<Move> { new Move(Direction.Right, 1) }, 2, 0, 0 },
-            new object[] { new List<Move> { new Move(Direction.Right, 2) }, 2, 0, 0 },
-            new object[] { new List<Move> { new Move(Direction.Right, 3) }, 2, 0, 1 },
-            new object[] { new List<Move> { new Move(Direction.Right, 7) }, 2, 1, 2 },
-            new object[] { new List<Move> { new Move(Direction.Right, 8) }, 2, 1, 3 },
-        };
+        public static readonly IEnumerable<object[]> _testLeftMove = [
+        
+            [new List<Move> { new Move(Direction.Left, 1) }, 2, 0, 0],
+            [new List<Move> { new Move(Direction.Left, 2) }, 2, 0, 0],
+            [new List<Move> { new Move(Direction.Left, 3) }, 2, 0, 1],
+            [new List<Move> { new Move(Direction.Left, 7) }, 2, 1, 3],
+            [new List<Move> { new Move(Direction.Left, 8) }, 2, 1, 2],
+        ];
+        public static readonly IEnumerable<object[]> _testRighttMove = [
+        
+            [new List<Move> { new Move(Direction.Right, 1) }, 2, 0, 0],
+            [new List<Move> { new Move(Direction.Right, 2) }, 2, 0, 0],
+            [new List<Move> { new Move(Direction.Right, 3) }, 2, 0, 1],
+            [new List<Move> { new Move(Direction.Right, 7) }, 2, 1, 2],
+            [new List<Move> { new Move(Direction.Right, 8) }, 2, 1, 3],
+        ];
         public static IEnumerable<object[]> _testMoves = [.._testLeftMove, .._testRighttMove];
 
 
